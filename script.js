15500d5ad8c357ef5a1f48fccd3bcead0f9624ba
usamafaheemAhmed <117355964+usamafaheemAhmed@users.noreply.github.com>
2023-05-30 00:44:39 +0500

    usama

--- script.js
+++ script.js
@@ -1,3 +1,19 @@
+function colorOptions(){
+  let variation = document.getElementById("variation").value;
+  if(variation=="timberLook"){
+    // -	Light Oak – Wild Olive – Jarrah – Western Red Cedar
+    document.getElementById("color").innerHTML="<option value='lightOak'>Light Oak</option><option value='wildOlive'>Wild Olive</option><option value='Jarrah'>Jarrah</option><option value='westernRedCedar'>Western Red Cedar</option>"
+  }
+  else if(variation=="powderCoated"){
+    // -	Classic Cream – Monument Satin – Pearl White Gloss – Black Satin – Shale Grey Matte – Surfmist – Woodland Grey Matte 
+    document.getElementById("color").innerHTML="<option value='classicCream'>Classic Cream</option><option value='monumentSatin'>Monument Satin</option><option value='pearlWhiteGloss'>Pearl White Gloss</option><option value='blackSatin'>Black Satin</option><option value='shaleGreyMatte'>Shale Grey Matte</option><option value='surfmist'>Surfmist</option><option value='woodlandGreyMatte '>Woodland Grey Matte </option>"
+  }
+  else if(variation=="customColor"){
+    // -	Basalt – Cottage Green – Deep Ocean – Dune – Evening Haze – Gully – Ironstone – Jasper – Manor Red – Pale Eucalypt – Paperbark – Wallaby – Windspray 
+    document.getElementById("color").innerHTML="   <option value='basalt'>Basalt</option><option value='classicCream'>Classic Cream</option><option value='cottageGreen'>Cottage Green</option><option value='cove'>Cove</option><option value='deepOcean'>Deep Ocean</option><option value='dune'>Dune</option><option value='eveningHaze'>Evening Haze</option><option value='gully'>Gully</option><option value='ironstone'>Ironstone</option><option value='jasper'>Jasper</option><option value='mangrove'>Mangrove</option><option value='manorRed'>Manor Red</option><option value='paleEucalypt'>Pale Eucalypt</option><option value='paperbark'>Paperbark</option><option value='terrain'>Terrain</option><option value='wallaby'>Wallaby</option><option value='windSpray'>Wind Spray</option>"
+  }
+}
+
 var increment = 1;
 var subIncrement = 1;
 
@@ -155,85 +171,173 @@ function calculate() {
   let channelAngleLefts = [];
   let channelAngleRights = [];
 
+
+
+
   let sumOfHeight =0,sumOfWidths =0,sumOfPreferredGaps =0;
 
   // what is heights user Entered
   for (let i = 1; i <= increment; i++) {
-    let isFilled =  parseFloat(document.getElementById("height" + i).value)||0;
-
-    if (isFilled == "") {
+    let isFilledHeight =  parseFloat(document.getElementById("height" + i).value)||0;
+    let isFilledWidth = parseFloat(document.getElementById("width" + i).value)||0;
+    let isFilledGap = parseFloat(document.getElementById("preferredHorizontalGap" + i).value)||0;
+    if (isFilledHeight == "" || isFilledWidth==""|| isFilledGap=="") {
       console.log("error");
       break;
     } else {
-      console.log("no error");
-      heights[i] = isFilled;
-      sumOfHeight += isFilled;
-
+      heights[i] = isFilledHeight;
+      widths[i] = isFilledWidth;
+      preferredHorizontalGaps[i] = isFilledGap;
     }
   }
   
-  //   console.log("heights ",JSON.stringify(heights));
-  // what is heights user Entered end
 
-  // what is widths user Entered
+  let numberOfSlats = [];
+  let actualGap = [];
+
   for (let i = 1; i <= increment; i++) {
-    let isFilled =  parseFloat(document.getElementById("width" + i).value)||0;
+         let a  = heights[i]/(preferredHorizontalGaps[i]+65);
+         let b  = Math.round(heights[i]/(preferredHorizontalGaps[i]+65))
+          if(a > b){
+            numberOfSlats[i] = b+1;
+          }else{
+            numberOfSlats[i] = b;
+          }
 
-    if (isFilled == "") {
-      console.log("error");
-      break;
-    } else {
-      console.log("no error");
-      widths[i] = isFilled;
-      sumOfWidths += isFilled;
-    }
+
+
+         actualGap[i] =( heights[i]/numberOfSlats[i])-65;
   }
-  //   console.log("widths ",JSON.stringify(widths));
-  // what is widths user Entered end
 
-  // what is preferredHorizontalGaps user Entered
-  for (let i = 1; i <= increment; i++) {
-    let isFilled = parseFloat(document.getElementById("preferredHorizontalGap" + i).value)||0;
 
-    if (isFilled == "") {
-      console.log("error");
-      break;
-    } else {
-      console.log("no error");
-      preferredHorizontalGaps[i] = isFilled;
-      sumOfPreferredGaps += isFilled;
-    }
-  }
-  //   console.log( "preferredHorizontalGaps ",JSON.stringify(preferredHorizontalGaps));
-  // what is preferredHorizontalGaps user Entered end
 
-  // what is preferredHorizontalGaps user Entered
-  for (let i = 1; i <= increment; i++) {
-    let isFilled = parseFloat(document.getElementById("channelAngleLeft" + i).value)||0;
 
-    if (isFilled == "") {
-      console.log("error");
-      break;
-    } else {
-      console.log("no error");
-      channelAngleLefts[i] = isFilled;
-    }
-  }
-  //   console.log( "channelAngleLefts ",JSON.stringify(channelAngleLefts));
-  // what is channelAngleLefts user Entered end
 
-  // what is channelAngleRights user Entered
-  for (let i = 1; i <= increment; i++) {
-    let isFilled = parseFloat(document.getElementById("channelAngleRight" + i).value)||0;
 
-    if (isFilled == "") {
-      console.log("error");
-      break;
-    } else {
-      console.log("no error");
-      channelAngleRights[i] = isFilled;
-    }
-  }
+
+
+
+
+  console.log(heights);
+  console.log(widths);
+  console.log(preferredHorizontalGaps);
+  console.log(numberOfSlats);
+  console.log(actualGap);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  // let sumOfHeight =0,sumOfWidths =0,sumOfPreferredGaps =0;
+
+  // // what is heights user Entered
+  // for (let i = 1; i <= increment; i++) {
+  //   let isFilled =  parseFloat(document.getElementById("height" + i).value)||0;
+
+  //   if (isFilled == "") {
+  //     console.log("error");
+  //     break;
+  //   } else {
+  //     console.log("no error");
+  //     heights[i] = isFilled;
+  //     sumOfHeight += isFilled;
+
+  //   }
+  // }
+  
+  // //   console.log("heights ",JSON.stringify(heights));
+  // // what is heights user Entered end
+
+  // // what is widths user Entered
+  // for (let i = 1; i <= increment; i++) {
+  //   let isFilled =  parseFloat(document.getElementById("width" + i).value)||0;
+
+  //   if (isFilled == "") {
+  //     console.log("error");
+  //     break;
+  //   } 
+  //   else
+  //    {
+  //     console.log("no error");
+  //     widths[i] = isFilled;
+  //     sumOfWidths += isFilled;
+  //   }
+  // }
+  // //   console.log("widths ",JSON.stringify(widths));
+  // // what is widths user Entered end
+
+  // // what is preferredHorizontalGaps user Entered
+  // for (let i = 1; i <= increment; i++) {
+  //   let isFilled = parseFloat(document.getElementById("preferredHorizontalGap" + i).value)||0;
+
+  //   if (isFilled == "") {
+  //     console.log("error");
+  //     break;
+  //   } else {
+  //     console.log("no error");
+  //     preferredHorizontalGaps[i] = isFilled;
+  //     sumOfPreferredGaps += isFilled;
+  //   }
+  // }
+  // //   console.log( "preferredHorizontalGaps ",JSON.stringify(preferredHorizontalGaps));
+  // // what is preferredHorizontalGaps user Entered end
+
+  // // what is preferredHorizontalGaps user Entered
+  // for (let i = 1; i <= increment; i++) {
+  //   let isFilled = parseFloat(document.getElementById("channelAngleLeft" + i).value)||0;
+
+  //   if (isFilled == "") {
+  //     console.log("error");
+  //     break;
+  //   } else {
+  //     console.log("no error");
+  //     channelAngleLefts[i] = isFilled;
+  //   }
+  // }
+  // //   console.log( "channelAngleLefts ",JSON.stringify(channelAngleLefts));
+  // // what is channelAngleLefts user Entered end
+
+  // // what is channelAngleRights user Entered
+  // for (let i = 1; i <= increment; i++) {
+  //   let isFilled = parseFloat(document.getElementById("channelAngleRight" + i).value)||0;
+
+  //   if (isFilled == "") {
+  //     console.log("error");
+  //     break;
+  //   } else {
+  //     console.log("no error");
+  //     channelAngleRights[i] = isFilled;
+  //   }
+  // }
+
+
+
+  // let sampleTestH = sumOfHeight;
+  // let sampleTestW = sumOfWidths;
+  // let sampleTestG = sumOfPreferredGaps;
+
+
+
+
+
+
 
   document.getElementById("variationResult").innerHTML= document.getElementById("variation").value;
   document.getElementById("colorResult").innerHTML= document.getElementById("color").value;
